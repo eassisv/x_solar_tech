41e446a169e67f0373b321ed3d27a50e27728979
Éverton Assis <eassis.vieira@gmail.com>
2020-05-16 21:15:24 -0300

    added address model and create-address migration

---
+++ database/migrations/20200516211029-create-address.js
@@ -0,0 +1,60 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('address', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'customer',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      cep: {
+        type: Sequelize.STRING(8),
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      others: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('address');
+  },
+};
---
+++ src/models/address.js
@@ -0,0 +1,26 @@
+const { Model } = require('sequelize');
+
+class Address extends Model {
+  static init(sequelize, DataTypes) {
+    super.init(
+      {
+        id: { type: DataTypes.INTEGER },
+        cep: { type: DataTypes.STRING, allowNull: false },
+        city: { type: DataTypes.STRING, allowNull: false },
+        state: { type: DataTypes.STRING, allowNull: false },
+        neighborhood: { type: DataTypes.STRING, allowNull: false },
+        street: { type: DataTypes.STRING, allowNull: false },
+        number: { type: DataTypes.INTEGER, allowNull: false },
+        others: { type: DataTypes.STRING },
+      },
+      { sequelize, tableName: 'address' },
+    );
+  }
+
+  static associate() {}
+}
+
+module.exports = (sequelize, DataTypes) => {
+  Address.init(sequelize, DataTypes);
+  return Address;
+};

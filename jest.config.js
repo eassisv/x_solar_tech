40a9c3bae949c60369d86271af5bb7e6894aef82
Éverton Assis <eassis.vieira@gmail.com>
2020-05-17 19:41:28 -0300

    fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -138,7 +138,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/tests/unit/sum.test.js'],
+  testMatch: ['**/tests/**/*.test.js'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [

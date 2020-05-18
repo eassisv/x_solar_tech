9fdb317d59f65d5250a4c511b3ff226f979d94f2
Éverton Assis <eassis.vieira@gmail.com>
2020-05-18 20:53:36 -0300

    added post method

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,8 +1,33 @@
-const { Customer } = require('../models');
+const { Customer, sequelize } = require('../models');
+
+const normalizeEmail = (email) => {
+  const [emailName, domain] = email.split('@');
+  return `${emailName}@${domain.toLowerCase()}`;
+};
 
 module.exports = {
   async list(_, res) {
     const customers = await Customer.findAll();
     res.json(customers);
+  },
+
+  async store(req, res) {
+    let transaction;
+    const data = {
+      ...req.data,
+      email: normalizeEmail(req.data.email),
+    };
+    try {
+      transaction = await sequelize.transaction();
+      const customer = await Customer.create(data, {
+        include: [Customer.Addresses],
+        transaction,
+      });
+      await transaction.commit();
+      res.status(201).json(customer);
+    } catch (err) {
+      await transaction.rollback();
+      res.status(500).json();
+    }
   },
 };
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const customerController = require('../controllers/customers');
+const validator = require('../middlewares/validator');
+const customerSchema = require('../schemas/customer');
 
 const router = Router();
 
 router.get('/', customerController.list);
+router.post('/', validator(customerSchema), customerController.store);
 
 module.exports = router;

5e741417ac6958fb1c1c950379d217ba2f8003ec
Éverton Assis <eassis.vieira@gmail.com>
2020-05-18 20:52:35 -0300

    updated validator middleware

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,9 +1,23 @@
+const getErrorObj = (error) => ({ type: error.type, error: error.errors });
+
 module.exports = (schema) => async (req, res, next) => {
   try {
-    const data = await schema.validate(req.body, { abortEarly: false });
+    const data = await schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
     req.data = data;
     next();
   } catch (err) {
-    return res.status(400).json(err);
+    const errors = err.inner.reduce(
+      (acc, error) => ({
+        ...acc,
+        [error.path]: acc[error.path]
+          ? [...acc[error.path], getErrorObj(error)]
+          : [getErrorObj(error)],
+      }),
+      {},
+    );
+    res.status(400).json({ errors });
   }
 };

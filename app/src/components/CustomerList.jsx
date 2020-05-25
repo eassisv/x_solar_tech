d7a828ddcecea6d8888c68923399543fb5cc523e
Éverton Assis <eassis.vieira@gmail.com>
2020-05-25 14:44:55 -0300

    added customer list component

---
+++ app/src/components/CustomerList.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import CustomerCard from "./CustomerCard";
+import { createFakeCustomerList } from "../mock";
+
+export default function CustomerList() {
+  const [customerList, setCustomerList] = useState([]);
+
+  useEffect(() => {
+    setCustomerList(createFakeCustomerList());
+  }, []);
+
+  return (
+    <div className="list__container">
+      {customerList.map((customer) => (
+        <CustomerCard key={customer.id} customer={customer} />
+      ))}
+    </div>
+  );
+}

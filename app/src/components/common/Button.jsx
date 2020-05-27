669cfb837deac0be32e77d4a083c087344bbe3a0
Éverton Assis <eassis.vieira@gmail.com>
2020-05-27 14:18:05 -0300

    added small buttons

--- app/src/components/common/Button.jsx
+++ app/src/components/common/Button.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../styles/Button.css";
 
-export default function Button({ children, variant, onClick }) {
-  const className = `button ${variant}`;
+export default function Button({ children, variant, small, onClick }) {
+  const smallClass = small ? " small" : "";
 
   return (
-    <button type="button" className={className} onClick={onClick}>
+    <button
+      type="button"
+      className={`button ${variant}${smallClass}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
@@ -15,9 +19,11 @@ export default function Button({ children, variant, onClick }) {
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   variant: PropTypes.string,
+  small: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
   variant: "primary",
+  small: false,
 };

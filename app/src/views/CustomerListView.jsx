99d200e09bc93dfdf623dbb4c95f57eaee77b0c0
Éverton Assis <eassis.vieira@gmail.com>
2020-05-28 15:58:47 -0300

    added search to customer list

--- app/src/views/CustomerListView.jsx
+++ app/src/views/CustomerListView.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/common/Button";
 import Container from "../components/common/Container";
 import CustomerList from "../components/CustomerList";
 import CustomerSearchBar from "../components/CustomerSearchBar";
 
 export default function CustomerListView() {
+  const [search, setSearch] = useState("");
+
   return (
     <Container>
       <Button variant="primary" to="/customers/new">
         Adicionar Cliente
       </Button>
-      <CustomerSearchBar onStopTyping={(value) => console.log(value)} />
-      <CustomerList />
+      <CustomerSearchBar onStopTyping={(value) => setSearch(value)} />
+      <CustomerList search={search} />
     </Container>
   );
 }

77a4ab1f86820729e8bd9bba2af42cc2f68245ae
Éverton Assis <eassis.vieira@gmail.com>
2020-05-28 15:57:51 -0300

    added customer search

--- app/src/components/CustomerList.jsx
+++ app/src/components/CustomerList.jsx
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CustomerCard from "./CustomerCard";
-import createFakeCustomerList from "../mock";
+import Spinner from "./common/Spinner";
 
-export default function CustomerList() {
+const instance = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
+
+export default function CustomerList({ search }) {
+  const [loading, setLoading] = useState(true);
   const [customerList, setCustomerList] = useState([]);
 
   useEffect(() => {
-    setCustomerList(createFakeCustomerList());
-  }, []);
+    async function fetchData(search) {
+      setLoading(true);
+      try {
+        const params = search !== "" ? { search } : {};
+        const response = await instance.get("/customers", {
+          params,
+        });
+        setCustomerList(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    }
+    fetchData(search);
+  }, [search]);
 
-  return (
+  return loading ? (
+    <Spinner />
+  ) : (
     <div className="list__container">
       {customerList.map((customer) => (
         <CustomerCard key={customer.id} customer={customer} />

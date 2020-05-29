c1583c988cfc9f5e2b64113dfa03965c274aa284
Éverton Assis <eassis.vieira@gmail.com>
2020-05-29 11:44:55 -0300

    added error message component

---
+++ app/src/components/common/ErrorMessage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/ErrorMessage.css";
+
+export default function ErrorMessage() {
+  return (
+    <div className="error-message">
+      Oops, ocorreu algum erro, tente novamente mais tarde...
+    </div>
+  );
+}
